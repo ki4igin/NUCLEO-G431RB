57450e6ff3633a71c2b606b123e31124aefd38ff
artem <ki4igin1992@outlook.com>
2021-07-01 23:39:57 +0300

    рефакторинг

--- LL_UART/Src/main.c
+++ LL_UART/Src/main.c
@@ -67,7 +67,7 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+  
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
